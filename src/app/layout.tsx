f9126f65a4737fc881250c6febbb5e92744e3f6b
Trevor Hecht <trevorwhecht@gmail.com>
2025-10-14 16:06:06 -0700

    fb pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Footer from "@/components/Layout/Footer"
 import NavBar from "@/components/Layout/NavBar"
 import { Geist, Geist_Mono } from "next/font/google";
 import { Providers } from "./providers";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,9 +32,34 @@ export default function RootLayout({
         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" 
       />
       <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+      {/* Meta Pixel Code */}
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '24844063038547006');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      {/* End Meta Pixel Code */}
     </head>
       <body className="relative bg-black">
-         <video 
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=24844063038547006&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        <video 
           autoPlay 
           muted 
           loop 
